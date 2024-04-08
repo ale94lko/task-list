8f889e75faacc7e934cbd12db5e1d91c1d491165
Fidel Alejandro Fernández Arias <47539231+ale94lko@users.noreply.github.com>
2024-04-08 08:48:13 -0400

    Add migrations, model and controller

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+}
