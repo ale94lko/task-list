e185ed7e5cb36ef6a895d5b4a54ce508a3e8187c
Fidel Alejandro Fernández Arias <47539231+ale94lko@users.noreply.github.com>
2024-04-08 23:28:27 -0400

    Add icons and some styles

--- resources/js/Pages/Tasks/Index.vue
+++ resources/js/Pages/Tasks/Index.vue
@@ -1,7 +1,28 @@
 <script setup>
 import { Head } from '@inertiajs/vue3';
+//import { Icon } from '@iconify/vue';
+
+defineProps({
+  tasks: {
+    type: Array,
+    required: true,
+  },
+});
+
 </script>
 
 <template>
     <Head title="Task list" />
+    <div class="container">
+        <div class="add">
+            <button class="add-button"></button>
+            <!-- <Icon icon="mdi-light:home" /> -->
+            <input type="text" placeholder="Type to add new task" />
+        </div>
+        <ul>
+            <li v-for="task in tasks" :key="task.id">
+                {{ task.content }}
+            </li>
+        </ul>
+    </div>
 </template>
